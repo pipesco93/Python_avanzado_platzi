33e70f773818f8f4669b8f199dd3ba0480f569b3
Felipe Escobar R <pipesco93@gmail.com>
2021-11-29 21:39:26 -0500

    Se agrega un ejemplo de closure

---
+++ closure_example.py
@@ -0,0 +1,13 @@
+def make_multiplier(x): #scope superior
+
+    def multiplier(n): #nested function
+        return x * n   #x variable de scope superior
+ 
+    return multiplier  #Retorno multiplier la nested function 
+
+times10 = make_multiplier(10) #crea una funcion make_multiplier qu recuerda al valor x como 10
+times4 = make_multiplier(4)
+
+print(times10(3)) # el 3 va a ser el n que paso de parámetro paea multiplier la nested function 
+print(times4(5))
+print(times10(times4(2)))
